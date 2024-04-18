cae6eb3a3af4e30f42a9169960d890f5f6df54b3
hereisSwapnil <swapnilskumars@gmail.com>
2024-04-18 15:44:21 +0530

    updated Verify Token middleware

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(401).send("You are not authenticated");
   }
