4f1cdef0f197e2db28e0f8745132d63839551e41
hereisSwapnil <swapnilskumars@gmail.com>
2024-05-11 16:30:15 +0530

    removed unwanted files

--- server/utils/firebase.js
+++
@@ -1,8 +0,0 @@
-const { initializeApp } = require("firebase/app");
-const firebaseConfig = require("../firebase/firebase.credentials.json");
-const { getStorage } = require("firebase/storage");
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-
-module.exports = storage;
