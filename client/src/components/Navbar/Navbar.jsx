be3a7392e3031e069e3fbcd9e4ed9e12ecc9240e
chetan <chetann8900@gmail.com>
2024-05-26 11:39:01 +0530

    FIX: Website does not loading and fix reload on links in footer

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -158,53 +158,16 @@ const Navbar = () => {
                             />
                           </svg>
                         )}
-                        {item.name}
+                        {item?.name}
                       </Link>
                     ))}
 
                     <div className="text-gray-200  mt-2 ml-2">
-                      Coins:{user.coins}
+                      Coins:{user?.coins}
                     </div>
                   </div>
                 </div>
-                {/* INPUT  */}
-                {/* <div className="relative mx-auto text-gray-600">
-                  <input
-                    className="border-2 border-gray-300 bg-white h-10 px-5 md:w-[600px] lg:w-[500px] w-[10vw] pr-8 rounded-lg text-sm focus:outline-none appearance-none"
-                    type="search"
-                    name="search"
-                    placeholder="Search"
-                    onKeyPress={(e) => {
-                      if (e.key === "Enter") {
-                        const searchTerm = e.target.value;
-                        window.location.href = `?search=${encodeURIComponent(
-                          searchTerm
-                        )}`;
-                      }
-                    }}
-                  />
-                  <button
-                    type="submit"
-                    className="absolute right-0 top-[-6px] mt-5 mr-4"
-                  >
-                    <svg
-                      className="text-gray-600 h-4 w-4 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      version="1.1"
-                      id="Capa_1"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 56.966 56.966"
-                      style={{ enableBackground: "new 0 0 56.966 56.966" }}
-                      xmlSpace="preserve"
-                      width="512px"
-                      height="512px"
-                    >
-                      <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                    </svg>
-                  </button>
-                </div> */}
+              
               </div>
               <div className="absolute inset-y-0 right-0 flex gap-3 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <Menu as="div" className="relative ml-3">
