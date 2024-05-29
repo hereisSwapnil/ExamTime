3152d45793583e0d0b268fda169ac269f5259d1e
Tapesh <tapesh.dua420@gmail.com>
2024-05-29 12:36:16 +0530

    added toggle functionality

---
+++ client/src/Context/ThemeContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(prevMode => !prevMode);
+    document.documentElement.classList.toggle('dark');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
