f8c656b0894016576e6effe0259c4defa08d27f4
chetan <chetann8900@gmail.com>
2024-05-29 17:01:51 +0530

    FIX: data fetch request is send to backend without user logedin causes error solved

--- client/src/Context/UserContext.jsx
+++ client/src/Context/UserContext.jsx
@@ -1,20 +1,12 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import { Loader } from "../components/Loader/Loader";
-import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
 
   const token = localStorage.getItem("token");
-
-  if (!token) {
-    navigate("/login");
-  }
 
   const config = {
     headers: {
@@ -24,28 +16,21 @@ export const UserContextProvider = ({ children }) => {
   };
 
   const getUser = async () => {
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/user/get`,
-        config
-      );
-      console.log(res.data);
-      if (res.data.isverified === true) {
-        setUser(res.data);
+    if (token) {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/user/get`,
+          config
+        );
+        console.log(res.data);
+        if (res.data.isverified === true) {
+          setUser(res.data);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
     }
   };
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
 
   return (
     <UserContext.Provider value={{ user, setUser, getUser }}>
